7fc84d2477a3268801a4918659c21c3b2a1acdf1
Arthur <arthurnunes.petee@gmail.com>
2022-01-31 21:31:47 -0300

    Commit 11

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     Customer customer = new Customer("Arthur");
-    Movie movie = new Movie("Interestelar", 10);
+    Movie movie = new Movie("Interestelar", 0);
     Rental rental = new Rental(movie, 7);
     System.out.println("Hello world!");
   }
